f858595a0a222120e9efb1124810d0f153c2777c
Symple44 <nicolas.dubain@gmail.com>
2024-12-11 00:32:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
-})
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.15:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  preview: {
+    port: 80
+  },
+  // Cette configuration est importante pour le routing
+  base: '/',
+});
