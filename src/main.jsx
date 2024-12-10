98d5e4304a0ae88c71345df07787033c1a4de2a6
Symple44 <nicolas.dubain@gmail.com>
2024-12-11 00:36:56 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ChatProvider } from './context/ChatContext';
 import Home from './pages/Home';
 import App from './App';
 import './styles/main.css';
