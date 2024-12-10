a071b274dbaad2ff3e5d50f690cc14505b5c0692
Symple44 <nicolas.dubain@gmail.com>
2024-12-11 00:24:34 +0100

    Delete src/chat directory

--- src/chat/MessageList.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/chat/MessageList.jsx
-const MessageList = ({ messages }) => (
-  <div className="flex-1 overflow-y-auto p-4">
-    {messages.map((message) => (
-      <MessageItem key={message.id} message={message} />
-    ))}
-  </div>
-);
