bf3b418fcc8cc0292a87ba09df5124b3c692e2a0
Symple44 <nicolas.dubain@gmail.com>
2024-12-11 21:22:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
       '/api': {
         target: 'http://192.168.0.15:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false
       },
       '/ws': {
