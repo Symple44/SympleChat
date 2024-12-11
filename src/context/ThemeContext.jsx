77a44dea8cd3205f2ad3c947bff0cf98759a7637
Symple44 <nicolas.dubain@gmail.com>
2024-12-11 13:50:18 +0100

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,1 +1,22 @@
+// src/context/ThemeContext.jsx
+import React, { createContext, useContext, useState } from 'react';
 
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      <div className={isDark ? 'dark' : ''}>
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
