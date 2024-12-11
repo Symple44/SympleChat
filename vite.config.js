5156deec2d16a2fff56f6a5f72a85f0710db0012
Symple44 <nicolas.dubain@gmail.com>
2024-12-11 10:05:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,22 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://192.168.0.15:8000',
         changeOrigin: true,
-        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false
       },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
+      '/ws': {
+        target: 'ws://192.168.0.15:8000',
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
 });
