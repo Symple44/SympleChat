b34a410a84c04db315bed805eea23bb36c4c1cdf
Symple44 <nicolas.dubain@gmail.com>
2024-12-12 00:59:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://192.168.0.15:8000/api',
