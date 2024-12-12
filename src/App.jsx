92e9f4f23f69b7fa0385bc3531d6ecc67fd44785
Symple44 <nicolas.dubain@gmail.com>
2024-12-12 17:41:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 // src/App.jsx
+import { useEffect } from 'react';
 import ChatContainer from './components/chat/ChatContainer';
-import useChat from './hooks/useChat'; 
+import useChat from './hooks/useChat';
+import { updatePageTitle } from './utils/pageTitle';
 
 function App() {
   const chatProps = useChat();
   
+  useEffect(() => {
+    updatePageTitle();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <ChatContainer {...chatProps} />
