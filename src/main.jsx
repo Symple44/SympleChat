b79f9375bbbe9b4e6b3dbb6d1a3e97b490add68a
Symple44 <nicolas.dubain@gmail.com>
2024-12-12 17:46:04 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ChatProvider } from './context/ChatContext';
 import { ThemeProvider } from './context/ThemeContext';
-import Home from './pages/Home';
+import { ChatProvider } from './context/ChatContext';
 import App from './App';
 import './styles/main.css';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/chat",
-    element: (
-      <ChatProvider>
-        <App />
-      </ChatProvider>
-    )
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('chat-root')).render(
   <React.StrictMode>
