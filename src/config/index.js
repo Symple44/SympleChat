cff48211d843d1c164166f56ffcda1faa5ffee4f
Symple44 <nicolas.dubain@gmail.com>
2024-12-12 11:01:50 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,9 +3,11 @@ const BACKEND_HOST = '192.168.0.15:8000';
 const HOST = isDev ? `http://${BACKEND_HOST}` : window.location.origin;
 
 export const config = {
-  API_BASE_URL: isDev ? `${HOST}/api` : `http://${BACKEND_HOST}/api`,
+  API_BASE_URL: `http://${BACKEND_HOST}/api`,  // Toujours pointer vers le backend
   WS_URL: `ws://${BACKEND_HOST}/ws/chat`,  // Toujours pointer vers le backend
   DEFAULT_USER_ID: 'oweo'
 };
+
+console.log('Config loaded:', config); // Pour debug
 
 export default config;
