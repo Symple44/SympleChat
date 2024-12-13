80d1fd6beff19be8bb72e768e47bf3cbca31d608
Symple44 <nicolas.dubain@gmail.com>
2024-12-13 22:42:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
-import { ChatProviderWithRouter } from './context/ChatContext';
+import { ChatProvider } from './context/ChatContext';
+import App from './App';
 import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('chat-root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <ChatProviderWithRouter />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <ChatProvider>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/session/:sessionId" element={<App />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </ChatProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
