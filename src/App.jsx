0f17e63440a04b8983f2171829fe73b3f6fc666b
Symple44 <nicolas.dubain@gmail.com>
2024-12-13 01:14:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 // src/App.jsx
 import { useEffect } from 'react';
 import ChatContainer from './components/chat/ChatContainer';
+import { useChatContext } from './context/ChatContext';
 import useChat from './hooks/useChat';
 import { updatePageTitle } from './utils/pageTitle';
 
-function App() {
-  const chatProps = useChat();
-  
-  useEffect(() => {
-    updatePageTitle();
-  }, []);
 
+function App() {
+  const { currentSessionId } = useChatContext();
+  
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
-      <ChatContainer {...chatProps} />
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col">
+      <Routes>
+        {/* Redirection vers la dernière session si pas d'URL spécifique */}
+        <Route path="/" element={
+          currentSessionId ? 
+            <Navigate to={`/session/${currentSessionId}`} replace /> : 
+            <ChatContainer />
+        } />
+        {/* Route pour une session spécifique */}
+        <Route path="/session/:sessionId" element={<ChatContainer />} />
+      </Routes>
     </div>
   );
 }
