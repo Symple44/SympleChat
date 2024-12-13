b8a1ec84d89bb94a194c8fed412bb926e3998779
Symple44 <nicolas.dubain@gmail.com>
2024-12-13 01:09:21 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import { ThemeProvider } from './context/ThemeContext';
 import { ChatProvider } from './context/ChatContext';
 import App from './App';
 import './styles/main.css';
+import { ChatProviderWithRouter } from './context/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('chat-root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <ChatProvider>
+      <ChatProviderWithRouter>
         <App />
-      </ChatProvider>
+      </ChatProviderWithRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
