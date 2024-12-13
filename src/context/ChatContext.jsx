f3a495b659ce7399420e87d1298cb589c9d0ec38
Symple44 <nicolas.dubain@gmail.com>
2024-12-13 16:42:06 +0100

    Update ChatContext.jsx

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -72,7 +72,9 @@ const router = createBrowserRouter([
     v7_normalizeFormMethod: true,
     v7_partialHydration: true,
     v7_fetcherPersist: true,
-    v7_relativeSplatPath: true
+    v7_relativeSplatPath: true,
+    v7_prependBasename: true,
+    v7_skipActionErrorRevalidation: true
   }
 });
 
