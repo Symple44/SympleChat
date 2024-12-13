e2469db70004d18c3a4a2f7a4fcdd650f45b7c51
Symple44 <nicolas.dubain@gmail.com>
2024-12-13 08:53:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from './context/ThemeContext';
-import { ChatProvider } from './context/ChatContext';
+import { ChatProviderWithRouter } from './context/ChatContext';
 import App from './App';
 import './styles/main.css';
-import { ChatProviderWithRouter } from './context/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('chat-root')).render(
   <React.StrictMode>
