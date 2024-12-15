355d25d704df2d8a10d57bccd5b2bd7ba7ee604b
Symple44 <nicolas.dubain@gmail.com>
2024-12-15 21:26:58 +0100

    Delete src directory

--- src/utils/dateFormatter.js
+++
@@ -1,9 +0,0 @@
-// utils/dateFormatter.js
-export const formatTimestamp = (timestamp) => {
-  const date = new Date(timestamp);
-  return new Intl.DateTimeFormat('fr-FR', {
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
-  }).format(date);
-};
--- src/utils/pageTitle.js
+++
@@ -1,8 +0,0 @@
-// src/utils/pageTitle.js
-import { config } from '../config';
-
-export const updatePageTitle = (suffix = '') => {
-  const baseTitle = config.APP.NAME;
-  const titleSuffix = suffix || config.APP.TITLE_SUFFIX;
-  document.title = `${baseTitle}${titleSuffix ? ` ${titleSuffix}` : ''}`;
-};
