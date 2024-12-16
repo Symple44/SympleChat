1d0e297b78c99f53a208bf650396640f43de04aa
Symple44 <nicolas.dubain@gmail.com>
2024-12-16 10:14:39 +0100

    Delete shared directory

--- shared/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/common/Input.jsx
-const Input = ({ ...props }) => (
-  <input
-    className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-    {...props}
-  />
-);
