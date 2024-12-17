6fdb2f88c34f61dff2c7037937ecbc9c37969f16
Symple44 <nicolas.dubain@gmail.com>
2024-12-17 20:45:33 +0100

    Update AppProvider.tsx

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,37 +1,21 @@
 // src/providers/AppProvider.tsx
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ErrorBoundary from '../shared/components/ErrorBoundary';
+import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from '@/shared/components/ErrorBoundary';
 import { ThemeProvider } from './ThemeProvider';
 import { SocketProvider } from './SocketProvider';
-import { ChatProvider } from './ChatProvider';
-import { routes } from '../config/routes.config';
+import ChatProvider from './ChatProvider';
+import MainRouter from '@/routes/MainRouter';
 
-const AppProvider: React.FC<{ children?: React.ReactNode }> = () => {
+const AppProvider: React.FC = () => {
   return (
     <ErrorBoundary>
       <BrowserRouter>
         <ThemeProvider>
           <SocketProvider>
             <ChatProvider>
-              <Routes>
-                {routes.map((route, idx) => (
-                  <Route
-                    key={route.path || idx}
-                    path={route.path}
-                    element={route.element}
-                  >
-                    {route.children?.map((childRoute, childIdx) => (
-                      <Route
-                        key={childRoute.path || childIdx}
-                        path={childRoute.path}
-                        element={childRoute.element}
-                      />
-                    ))}
-                  </Route>
-                ))}
-              </Routes>
+              <MainRouter />
             </ChatProvider>
           </SocketProvider>
         </ThemeProvider>
